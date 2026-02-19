678cea814a6aa6036674453b838328114577670a
Prathmesh Kasabekar <kasabekarprathmesh@gmail.com>
2026-02-19 09:03:16 +0530

    first commit

---
+++ global.java
@@ -0,0 +1,23 @@
+public class global {
+    public class vairbale {
+        byte a = 127;
+        short b = 1234;
+        int c = 321654;
+        long d = 646896615;
+        float e = 42.65f;
+        double f = 642.5161;
+         String g = "efbkj";
+         boolean h = true;
+    }
+public static void main(String[] args) {
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+         System.out.println(d);
+         System.out.println(e);
+         System.out.println(f);
+         System.out.println(g);
+         System.out.println(h);
+    }
+}
+
